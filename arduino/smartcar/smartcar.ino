bca89107f954e0d29ed42e450a40eaee9f5c7022
JinaDawood <72136631+JinaDawood@users.noreply.github.com>
2021-04-12 12:43:57 +0200

    Basic movements and speed velocity
    * Created methods so the car can go, stop and reverse as specified in #5
    
    * Created a method so the car can accelerate and decelerate 10% from 10 -100.
    
    * Created a method to decide which of the movements will execute.
    
    * This pull request made us aware of one bug in the code (see issue #15).
    
    Closes: #5
    Resolves: #6
    Co-Authored-By: Mozand <81272931+Mozand@users.noreply.github.com>
    Co-Authored-By: jousifkdawood <81232269+jousifkdawood@users.noreply.github.com>

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -19,6 +19,36 @@ DifferentialControl control{leftMotor, rightMotor};
 SimpleCar car{control};
 SR04 sensor(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
+<<<<<<< Updated upstream
+=======
+ void stopVehicle(){
+  car.setSpeed(0);
+ }
+
+ void goForward(int speed){
+  car.setSpeed(speed);
+ }
+
+ void goBackward(int speed){
+  car.setSpeed(speed);
+ }
+ 
+ void decelerate(int curSpeed){ // start at 50 
+   int targetSpeed = curSpeed - changeSpeed; // decelerate by 10 
+   if (currentSpeed > minSpeed){
+    car.setSpeed(targetSpeed); // sets the speed to 40 
+   currentSpeed = targetSpeed; // sets the current 40  
+   }  
+ }
+ void accelerate(int curSpeed){
+  int targetSpeed = curSpeed + changeSpeed;
+   if (currentSpeed < maxSpeed){
+  car.setSpeed(targetSpeed);
+  currentSpeed = targetSpeed;
+   }
+ }
+
+>>>>>>> Stashed changes
 void setup() {
   Serial.begin(9600);
   car.setSpeed(cruiseSpeed);
