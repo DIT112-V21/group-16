d2f9d81ad389ca610531547351816212cfb62f8c
JinaDawood <72136631+JinaDawood@users.noreply.github.com>
2021-04-17 00:06:56 +0200

    test

---
+++ app/src/main/java/com/example/firstapp/CleaningOptionActivity.kt
@@ -0,0 +1,32 @@
+package com.example.firstapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View.inflate
+import com.example.firstapp.databinding.ActivityCleaningOptionBinding
+import com.example.firstapp.databinding.ActivityCleaningOptionBinding.inflate
+import com.example.firstapp.databinding.ActivityMainBinding
+
+class CleaningOptionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCleaningOptionBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_cleaning_option)
+
+        val actionBar = supportActionBar
+        actionBar!!.title = ""
+
+        binding = inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.manual.setOnClickListener{
+            binding.manual.text = ""
+            startActivity(Intent(this,ManualOptionActivity::class.java))
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.firstapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firstapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+   private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       // setContentView(R.layout.activity_main)
+        val actionBar = supportActionBar
+        actionBar!!.title = ""
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startCleaning.setOnClickListener {
+            binding.startCleaning.text = "start"
+            startActivity(Intent(this,CleaningOptionActivity::class.java))
+        }
+
+
+    }
+}
