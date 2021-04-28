b3ae9f0320a6ac1283e5cb6f75041d0a97c1b8a9
JinaDawood <72136631+JinaDawood@users.noreply.github.com>
2021-04-28 22:41:11 +0200

    arduino mqtt
    implemented MQTT connection in arduino
    
    resolves issue #28
    
    Co-Authored-By: Young799 <81288695+Young799@users.noreply.github.com>

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,14 +1,38 @@
 #include <Smartcar.h>
+#include <vector>
+#include <MQTT.h>
+#include <WiFi.h>
+
+#ifdef __SMCE__
+#include <OV767X.h>
+#endif
+
+#ifndef  __SMCE__
+WiFiClient net;
+#endif
+MQTTClient mqtt;
+
+
 
 const int fSpeed   = 25;  // 25% of the full speed forward
 const int bSpeed   = -10; // 10% of the full speed backward
 const int changeSpeed = 10;
 const int maxSpeed = 100;
 const int minSpeed = 10;
-const int triggerDist = 200;
+const int triggerDist = 100;
 const int lDegrees = -80; // degrees to turn left
 const int rDegrees = 80; // degrees to turn right
+const int SERVO_PIN = 8;
+const int ESC_PIN = 9;
+const int BACK_IR_PIN =3;
+const int FRONT_IR_PIN =3;
 
+
+ 
+//ServoMotor steering (SERVO_PIN);
+//ServoMotor throttling (ESC_PIN);
+//AckermanControl control(steering, throttling);
+ 
 int currentSpeed = fSpeed;
 
 unsigned short TRIGGER_PIN = 6;
@@ -16,11 +40,49 @@ unsigned short ECHO_PIN = 7;
 const unsigned int MAX_DISTANCE = 1000;
 
 ArduinoRuntime arduinoRuntime;
-BrushedMotor leftMotor{arduinoRuntime, smartcarlib::pins::v2::leftMotorPins};
-BrushedMotor rightMotor{arduinoRuntime, smartcarlib::pins::v2::rightMotorPins};
-DifferentialControl control{leftMotor, rightMotor};
-SimpleCar car{control};
-SR04 sensor(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+SimpleCar car(control);
+SR04 sensor(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
+GP2D120 backIRSensor(arduinoRuntime,BACK_IR_PIN);  //measure in short distances
+GP2D120 frontIRSensor(arduinoRuntime,FRONT_IR_PIN);
+
+
+
+void setup() {
+  Serial.begin(9600);
+#ifndef __SMCE__ 
+  mqtt.begin("aerostun.dev", 1883, WiFi);  // Will connect to TA server
+  //mqtt.begin(WiFi); // to localhost
+#else
+  mqtt.begin(net);
+#endif
+  if (mqtt.connect("arduino", "public", "public")) {
+    mqtt.subscribe("/smartcar/control/#", 1);
+    mqtt.onMessage([](String topic, String message) {
+      if (topic == "/smartcar/control/throttle") {
+        car.setSpeed(message.toInt());
+      } else if (topic == "/smartcar/control/steering") {
+        car.setAngle(message.toInt());
+      } else {
+        Serial.println(topic + " " + message);
+      }
+    });
+  }
+}
+
+
+ void loop() {
+   if (mqtt.connected()) {
+     mqtt.loop();}
+    
+    handleInput();
+   obstacleAvoidance();
+   ObstacleAvoidanceBack();
+
+   }
+
 
  void stopVehicle(){
   car.setSpeed(0);
@@ -38,14 +100,14 @@ SR04 sensor(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
  
  void turnLeft(){
     car.setAngle(lDegrees);
-		delay(2000);
-		car.setAngle(0);
+    delay(2000);
+    car.setAngle(0);
  }
 
  void turnRight(){
  car.setAngle(rDegrees);
-	delay(2000);
-	car.setAngle(0);
+  delay(2000);
+  car.setAngle(0);
  }
  
  void decelerate(int curSpeed){ // start at 50 
@@ -64,18 +126,16 @@ SR04 sensor(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
    }
  }
 
-void setup() {
-  Serial.begin(9600);
-}
-void loop() {
- 
+//void setup() {
+  //Serial.begin(9600);
+//}
+
+/* void loop() {
    handleInput();
-   
-  unsigned int distance = sensor.getDistance();
-  if (distance > 0 && distance < triggerDist && currentSpeed >= 0 ){ //third condition added that checks if the car is moving forward.
-      car.setSpeed(0);
-  }
-} 
+  // setSpeedAndAngle();
+   obstacleAvoidance();
+   ObstacleAvoidanceBack();
+} */
 
 void handleInput(){ // handle serial input if there is any
     if (Serial.available()){
@@ -83,28 +143,28 @@ void handleInput(){ // handle serial input if there is any
                                     // the last entry
         switch (input) {
         case 'f': // go ahead in medium speed 
-			if (currentSpeed>0){
-				goForward(currentSpeed); // starts on 50 %, contiunes based on the speed before it stopped.
-			}else{ // 
-				goForward(fSpeed);
-			}
+      if (currentSpeed>0){
+        goForward(currentSpeed); // starts on 50 %, contiunes based on the speed before it stopped.
+      }else{ // 
+        goForward(fSpeed);
+      }
             break;
         case 'b': // go back 
-			if (currentSpeed<0){
-				goBackward(currentSpeed); // starts on 50 %, contiunes based on the speed before it stopped.
-			}else{
-				goBackward(bSpeed);
-			}
+      if (currentSpeed<0){
+        goBackward(currentSpeed); // starts on 50 %, contiunes based on the speed before it stopped.
+      }else{
+        goBackward(bSpeed);
+      }
             break;
         case 's': // stop 
             stopVehicle();
             break;
         case 'l': // turn left
-		        turnLeft();
-			      break;
-		    case 'r': // turn right
-			      turnRight();
-			      break;
+            turnLeft();
+            break;
+        case 'r': // turn right
+            turnRight();
+            break;
         case 'd': // the car decelerates
             decelerate(currentSpeed);
             break;
@@ -114,5 +174,38 @@ void handleInput(){ // handle serial input if there is any
         default: // if you receive something that           you don't know, just stop
             stopVehicle();
         } 
+    }
+}
+
+void obstacleAvoidance(){
+   int distance = sensor.getDistance();
+  if (distance > 0 && distance < triggerDist && currentSpeed >= 0 ){ //third condition added that checks if the car is moving forward.
+      car.setSpeed(0);
+  }
+
+}
+void ObstacleAvoidanceBack(){
+    int backDistance=backIRSensor.getDistance();
+    if(backDistance >0 && backDistance <triggerDist){
+      car.setSpeed(0);
+    }
+
+}
+
+void setSpeedAndAngle()
+{
+    // handle serial input if there is any
+    if (Serial.available())
+    {
+        String input = Serial.readStringUntil('\n');
+        if (input.startsWith("m"))
+        {  int throttle = input.substring(1).toInt();
+            car.setSpeed(throttle);
+        }
+        else if (input.startsWith("t"))
+        {
+            int deg = input.substring(1).toInt();
+            car.setAngle(deg);
+        }
     }
 }
