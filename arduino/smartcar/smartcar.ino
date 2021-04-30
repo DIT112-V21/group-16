e47be81f69261aa2e4e3ff45525c59c4153aa324
alandr <alandr@student.chalmers.se>
2021-04-30 14:01:18 +0200

    small fix again

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -130,8 +130,6 @@ void loop() {
     mqtt.loop();
   }
 
-  handleInput();
-
     unsigned int distance = sensor.getDistance();
   if (distance > 0 && distance < triggerDist && currentSpeed >= 0 ){ //third condition added that checks if the car is moving forward.
       car.setSpeed(0);
