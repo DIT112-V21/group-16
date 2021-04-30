dcb97783a28fbc4a6aa56297b2cb32eb8f9a7d08
Mozand <Tanhayetanha6@gmail.com>
2021-04-30 22:26:35 +0200

    Add turn on spot methods

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -5,15 +5,17 @@ const int bSpeed   = -10; // 10% of the full speed backward
 const int changeSpeed = 10;
 const int maxSpeed = 100;
 const int minSpeed = 10;
-const int triggerDist = 200;
-const int lDegrees = -80; // degrees to turn left
-const int rDegrees = 80; // degrees to turn right
+const int triggerDist = 0;
+const int lDegrees = -90; // degrees to turn left
+const int rDegrees = 90; // degrees to turn right
 
-int currentSpeed = fSpeed;
+int currentSpeed = 0;
 
 unsigned short TRIGGER_PIN = 6;
 unsigned short ECHO_PIN = 7;
 const unsigned int MAX_DISTANCE = 1000;
+
+
 
 ArduinoRuntime arduinoRuntime;
 BrushedMotor leftMotor{arduinoRuntime, smartcarlib::pins::v2::leftMotorPins};
@@ -22,8 +24,11 @@ DifferentialControl control{leftMotor, rightMotor};
 SimpleCar car{control};
 SR04 sensor(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
+
+
  void stopVehicle(){
   car.setSpeed(0);
+  currentSpeed = 0; 
  }
 
  void goForward(int speed){
@@ -42,12 +47,33 @@ SR04 sensor(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 		car.setAngle(0);
  }
 
- void turnRight(){
+ void turnRight()
+ {
  car.setAngle(rDegrees);
 	delay(2000);
 	car.setAngle(0);
  }
  
+ void turnLeftWhenStoped()  // when car doesn't move it will turn on spot to left
+ {
+    car.setSpeed(fSpeed);
+    currentSpeed = fSpeed;
+    car.setAngle(lDegrees);
+		delay(6000);
+		car.setAngle(0);
+    stopVehicle();
+ }
+
+  void turnRightWhenStoped() // when car doesn't move it will turn on spot to right
+ {
+    car.setSpeed(fSpeed);
+    currentSpeed = fSpeed;
+    car.setAngle(rDegrees);
+		delay(6000);
+		car.setAngle(0);
+    stopVehicle();
+ }
+
  void decelerate(int curSpeed){ // start at 50 
    int targetSpeed = curSpeed - changeSpeed; // decelerate by 10 
    if (currentSpeed > minSpeed){
@@ -72,7 +98,8 @@ void loop() {
    handleInput();
    
   unsigned int distance = sensor.getDistance();
-  if (distance > 0 && distance < triggerDist && currentSpeed >= 0 ){ //third condition added that checks if the car is moving forward.
+  
+  if (distance > 0 && distance < triggerDist && currentSpeed >= 0  ){ //third condition added that checks if the car is moving forward.
       car.setSpeed(0);
   }
 } 
@@ -81,14 +108,15 @@ void handleInput(){ // handle serial input if there is any
     if (Serial.available()){
         char input = Serial.read(); // read everything that has been received so far and log down
                                     // the last entry
-        switch (input) {
-        case 'f': // go ahead in medium speed 
-			if (currentSpeed>0){
-				goForward(currentSpeed); // starts on 50 %, contiunes based on the speed before it stopped.
-			}else{ // 
-				goForward(fSpeed);
-			}
-            break;
+        switch (input)
+         {
+            case 'f': // go ahead in medium speed 
+		    	    if (currentSpeed>0){
+				        goForward(currentSpeed); // starts on 50 %, contiunes based on the speed before it stopped.
+		    	    }else{ // 
+				        goForward(fSpeed);
+			        }
+          break;
         case 'b': // go back 
 			if (currentSpeed<0){
 				goBackward(currentSpeed); // starts on 50 %, contiunes based on the speed before it stopped.
@@ -100,10 +128,25 @@ void handleInput(){ // handle serial input if there is any
             stopVehicle();
             break;
         case 'l': // turn left
-		        turnLeft();
+		        if(currentSpeed>0)
+            {
+              turnLeft();
+            }
+            else
+            {
+              turnLeftWhenStoped();
+            }
+            
 			      break;
 		    case 'r': // turn right
-			      turnRight();
+			      if(currentSpeed>0)
+            {
+              turnRight();
+            }
+            else
+            {
+              turnRightWhenStoped();
+            }
 			      break;
         case 'd': // the car decelerates
             decelerate(currentSpeed);
