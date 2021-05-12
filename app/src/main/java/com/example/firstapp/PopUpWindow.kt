7efc8ef8925b7c27c1f749bc42527a2ccd964061
JinaDawood <72136631+JinaDawood@users.noreply.github.com>
2021-05-13 01:06:54 +0200

    fixing build problems

--- app/src/main/java/com/example/firstapp/PopUpWindow.kt
+++ app/src/main/java/com/example/firstapp/PopUpWindow.kt
@@ -1,147 +1,24 @@
 package com.example.firstapp
 
-import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.widget.*
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import org.eclipse.paho.client.mqttv3.*
+import com.example.firstapp.MQTT.MqttHandler
+
 
 class PopUpWindow : AppCompatActivity() {
-    private val TAG = "firstapp"
-    private val EXTERNAL_MQTT_BROKER = "aerostun.dev"
-    private val LOCALHOST = "10.0.2.2"
-    private val MQTT_SERVER = "tcp://$EXTERNAL_MQTT_BROKER:1883"
-    private val IMAGE_WIDTH = 320
-    private val IMAGE_HEIGHT = 240
-    private val QOS = 1
 
-    private var mMqttClient: MqttClient? = null
-    private var isConnected = false
-    private var mCameraView: ImageView? = null
+     private var mqttHandler: MqttHandler? = null
+     private var mCameraView : ImageView? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+  override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pop_up_window)
-        mMqttClient = MqttClient(getApplicationContext(), MQTT_SERVER, TAG)
-        connectToMqttBroker()
+
+        mqttHandler = MqttHandler(this.applicationContext, mCameraView)
+        mqttHandler!!.connectToMqttBroker()
+
+        mCameraView = findViewById(R.id.camera)
     }
-
-
-        override fun onResume() {
-        super.onResume()
-        connectToMqttBroker()
-    }
-
-        override fun onPause() {
-        super.onPause()
-        mMqttClient!!.disconnect(object : IMqttActionListener {
-            override fun onSuccess(asyncActionToken: IMqttToken) {
-                Log.i(TAG, "Disconnected from broker")
-            }
-
-            override fun onFailure(asyncActionToken: IMqttToken, exception: Throwable) {}
-        })
-    }
-
-    private fun connectToMqttBroker() {
-        if (!isConnected)
-        {
-            mMqttClient?.connect(object : IMqttActionListener {
-                override fun onSuccess(asyncActionToken: IMqttToken) {
-                    isConnected = true
-
-                    val successfulConnection = "Connected to MQTT broker";
-                    Log.i(TAG, successfulConnection);
-                    Toast.makeText(applicationContext, successfulConnection, Toast.LENGTH_SHORT)
-                        ?.show()
-                    mMqttClient?.subscribe("/smartcar/group16/camera", QOS, null);
-                }
-
-                override fun onFailure(asyncActionToken: IMqttToken, exception: Throwable) {
-                    val failedConnection = "Failed to connect to MQTT broker"
-                    Log.e(TAG, failedConnection)
-                    Toast.makeText(applicationContext, failedConnection, Toast.LENGTH_SHORT).show()
-                }
-            }, object : MqttCallback {
-                override fun connectionLost(cause: Throwable) {
-                    isConnected = false
-                    val connectionLost = "Connection to MQTT broker lost"
-                    Log.w(TAG, connectionLost)
-                    Toast.makeText(applicationContext, connectionLost, Toast.LENGTH_SHORT).show();
-                }
-
-                @Throws(Exception::class)
-                override fun messageArrived(topic: String, message: MqttMessage) {
-                    if (topic == "/smartcar/group16/camera") {
-                        val bm = Bitmap.createBitmap(
-                            IMAGE_WIDTH,
-                            IMAGE_HEIGHT,
-                            Bitmap.Config.ARGB_8888
-                        )
-
-                        val payload = message.payload
-                        val colors = IntArray(IMAGE_WIDTH * IMAGE_HEIGHT)
-                        for (ci in colors.indices) {
-                            val r = payload[3 * ci]
-                            val g = payload[3 * ci + 1]
-                            val b = payload[3 * ci + 2]
-                            colors[ci] = Color.rgb(r.toInt(), g.toInt(), b.toInt())
-                        }
-                        bm.setPixels(colors, 0, IMAGE_WIDTH, 0, 0, IMAGE_WIDTH, IMAGE_HEIGHT);
-                        mCameraView!!.setImageBitmap(bm)
-                    } else {
-                        Log.i(TAG, "[MQTT] Topic: $topic | Message: $message")
-                    }
-                }
-
-                override fun deliveryComplete(token: IMqttDeliveryToken) {
-                    Log.d(TAG, "Message delivered");
-                }
-            })
-        }
-    }
-
-
-    fun showPopupWindow(view: View) {
-        //Create a View object yourself through inflater
-        val inflater =
-            view.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val popupView = inflater.inflate(R.layout.pop_up_window, null)
-
-        //Specify the length and width through constants
-        val width = LinearLayout.LayoutParams.MATCH_PARENT
-        val height = LinearLayout.LayoutParams.MATCH_PARENT
-
-        //Make Inactive Items Outside Of PopupWindow
-        val focusable = true
-
-        //Create a window with our parameters
-        val popupWindow = PopupWindow(popupView, width, height, focusable)
-
-        //Set the location of the window on the screen
-        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
-
-        //Initialize the elements of our window, install the handler
-
-        mCameraView = findViewById(R.id.camera);
-        //var test1 = popupView.findViewById<ImageView>(R.id.camera)
-        //mCameraView = test1
-        val buttonEdit = popupView.findViewById<Button>(R.id.messageButton)
-        buttonEdit.setOnClickListener { //As an example, display the message
-            Toast.makeText(view.context, "Wow, popup action button", Toast.LENGTH_SHORT).show()
-        }
-
-        //Handler for clicking on the inactive zone of the window
-        popupView.setOnTouchListener { v, event -> //Close the window when clicked
-            popupWindow.dismiss()
-            true
-        }
-    }
-
 }
