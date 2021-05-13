28ee6fb627eb1f79d0f847118e1a74fe5fe57b63
gusmagadc <81155132+gusmagadc@users.noreply.github.com>
2021-05-13 23:43:15 +0200

    Auto Mode

--- app/src/main/java/com/example/firstapp/AutoOptionActivity.kt
+++ app/src/main/java/com/example/firstapp/AutoOptionActivity.kt
@@ -2,10 +2,15 @@ package com.example.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+
 
 class AutoOptionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auto_option)
+
     }
+
 }
