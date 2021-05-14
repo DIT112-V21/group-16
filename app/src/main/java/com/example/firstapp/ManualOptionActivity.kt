cdbb98c487ef38b94ea9692aae95d2507ff6c84c
JinaDawood <72136631+JinaDawood@users.noreply.github.com>
2021-05-14 02:07:50 +0200

    - Updated the layout for ManualOptionActivity.kt and CleaningOptionActivity. Its now possible to view speed, distance and ultrasound front distance in the manual screen.
    - Changed the layout of the joystick and enabled usage of the joystick.
    
    - added the camera view in the popup window which is in the manual screen.
    
    closes #34, #65 and #64

--- app/src/main/java/com/example/firstapp/ManualOptionActivity.kt
+++ app/src/main/java/com/example/firstapp/ManualOptionActivity.kt
@@ -1,11 +1,11 @@
 package com.example.firstapp
-import android.R.attr
 import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firstapp.MQTT.MqttHandler
 import io.github.controlwear.virtual.joystick.android.JoystickView
+import io.github.controlwear.virtual.joystick.android.JoystickView.OnMoveListener
 
 
 class ManualOptionActivity : AppCompatActivity() {
@@ -16,70 +16,87 @@ class ManualOptionActivity : AppCompatActivity() {
     private val THROTTLE_CONTROL = "/smartcar/group16/control/throttle"
     private val STEERING_CONTROL = "/smartcar/group16/control/steering"
     private val QOS = 0
-    private val FORWARD_SPEED = 40
-    private val ANGLE = 0
-
-    private var forwardBtn: Button? = null
-    private var backwardBtn: Button? = null
-    private var stopBtn: Button? = null
-    private var leftBtn: Button? = null
-    private var rightBtn: Button? = null
-
+    private val REVERSE = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manual_option)
 
         val  mTraveledDistance : TextView = findViewById(R.id.distance)
+        val  mFront : TextView = findViewById(R.id.front)
+        val  mSpeed: TextView = findViewById(R.id.speed)
 
         val actionBar = supportActionBar
         actionBar!!.title = ""
 
         //mqtt car handler
-        mqttHandler = MqttHandler(this.applicationContext, mTraveledDistance)
+        mqttHandler = MqttHandler(this.applicationContext, mTraveledDistance, mFront, mSpeed)
         mqttHandler!!.connectToMqttBroker()
 
-        // transition to the popup window when clicking on the camera button
+        // Transition to the popup window when clicking on the camera button
         mCameraButton = findViewById(R.id.camera)
         mCameraButton?.setOnClickListener {
             val window = PopupWindow(this.applicationContext)
             val view = layoutInflater.inflate(R.layout.pop_up_window, null)
             window.contentView = view
             val imageView = view.findViewById<ImageView>(R.id.cameraView)
+
+            mqttHandler = MqttHandler(this.applicationContext, imageView)
+            mqttHandler!!.connectToMqttBroker()
+
             imageView.setOnClickListener{
                 window.dismiss()
             }
             window.showAsDropDown(mCameraButton)
         }
 
-        // this joystick is adapted from: https://github.com/controlwear/virtual-joystick-android
-        val joystickLeft = findViewById<View>(R.id.joystickView_left) as JoystickView
-        joystickLeft.setOnMoveListener { angle, strength ->
+        // This joystick is adapted from: https://github.com/controlwear/virtual-joystick-android
+        val joystick = findViewById<View>(R.id.joystickView_left) as JoystickView
+        joystick.setOnMoveListener(object : OnMoveListener {
+            var currentSpeed = 0
+            var currentAngle = 0
+            override fun onMove(angle: Int, strength: Int) {
+                val newAngle: Int = updateAngle(angle)
+                val newSpeed: Int = updateSpeed(strength, angle)
+                move(newSpeed, newAngle, currentSpeed, currentSpeed)
+                currentAngle = newAngle
+                currentSpeed = newSpeed
+            }
+        })
+    }
 
+    private fun move(
+        newSpeed: Int,
+        newAngle: Int,
+        currentAngle: Int,
+        currentSpeed: Int
+    ) {
+        var newAngle = newAngle
+        if (newAngle != currentAngle || newSpeed != currentSpeed) {
+            if (newSpeed == 0) newAngle = 0
+            mqttHandler?.publish(STEERING_CONTROL, Integer.toString(newAngle), QOS, null)
+            mqttHandler?.publish(THROTTLE_CONTROL, Integer.toString(newSpeed), QOS, null)
         }
-
     }
 
+    private fun updateAngle(angle: Int): Int {
+        return if (angle in 90..180) { // left
+        90 - angle
+    } else if (angle in 0..89) { // right
+        90 - angle
+    } else if (angle > 0 && angle >= 270) { // back right
+        angle - 270
+    } else { // back left
+        angle - 270
+    }
+    }
 
-
-  /*  fun forward(view: View) {
-        forwardBtn = findViewById(R.id.forward)
-        mqttHandler!!.forward(forwardBtn)
+    private fun updateSpeed(strength: Int, angle: Int): Int {
+        val newSpeed: Double = if (angle <= 180) {
+            strength * 0.8
+        } else {
+            strength * 0.8 * REVERSE
+        }
+        return newSpeed.toInt()
     }
-    fun backward(view: View) {
-        backwardBtn = findViewById(R.id.backward)
-        mqttHandler!!.backward(backwardBtn)
-    }
-    fun forwardRight(view: View) {
-        rightBtn = findViewById(R.id.rightForward)
-        mqttHandler!!.forwardRight(rightBtn)
-    }
-    fun forwardLeft(view: View) {
-        leftBtn = findViewById(R.id.leftForward)
-        mqttHandler!!.forwardLeft(leftBtn)
-    }
-    fun stop(view: View) {
-        stopBtn = findViewById(R.id.stop)
-        mqttHandler!!.stop(stopBtn)
-    }*/
 }
