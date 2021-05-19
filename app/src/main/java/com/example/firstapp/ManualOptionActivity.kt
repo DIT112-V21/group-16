1cd14b6d1043a9d615733ba3a9fc261c8e8bf934
Young799 <guschoeu@student.gu.se>
2021-05-20 00:19:29 +0200

    Cleaning start button updated to enable to pause/restart process. Toast message will be shown when the status is 100%.

--- app/src/main/java/com/example/firstapp/ManualOptionActivity.kt
+++ app/src/main/java/com/example/firstapp/ManualOptionActivity.kt
@@ -1,34 +1,54 @@
 package com.example.firstapp
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.HandlerCompat.postDelayed
 import com.example.firstapp.MQTT.MqttHandler
 import io.github.controlwear.virtual.joystick.android.JoystickView
 import io.github.controlwear.virtual.joystick.android.JoystickView.OnMoveListener
 
 
+
 class ManualOptionActivity : AppCompatActivity() {
 
     private var mqttHandler: MqttHandler? = null
-    private var mCameraButton : ImageButton? = null
-    private var progressBar: ProgressBar? = null
+    private var mCameraButton: ImageButton? = null
     private val THROTTLE_CONTROL = "/smartcar/group16/control/throttle"
     private val STEERING_CONTROL = "/smartcar/group16/control/steering"
     private val QOS = 0
     private val REVERSE = -1
 
+    //progress Bar attribute
+    //private var mPbText: TextView? = null
+    //
+    var isStarted = false
+    var progressStatus = 0
+    var handler: Handler? = null
+
+
+
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manual_option)
+        var progressBar: ProgressBar = findViewById(R.id.progressBar)
+        val btn1: Button = findViewById(R.id.cleaning_start)
+        val btn2: Button = findViewById(R.id.empty_b)
+        val mTraveledDistance: TextView = findViewById(R.id.distance)
+        val mSpeed: TextView = findViewById(R.id.speed)
+        val mFront: TextView = findViewById(R.id.front)
+       // val toggle: ToggleButton = findViewById(R.id.start)
 
-        val  mTraveledDistance : TextView = findViewById(R.id.distance)
-        val  mSpeed: TextView = findViewById(R.id.speed)
-        val  mFront : TextView = findViewById(R.id.front)
+        //val mPbText: TextView = findViewById(R.id.bagfull)
 
         val actionBar = supportActionBar
         actionBar!!.title = ""
+        //var handler = Handler()
+
 
         //mqtt car handler
         mqttHandler = MqttHandler(this.applicationContext, mTraveledDistance, mSpeed, mFront)
@@ -45,48 +65,40 @@ class ManualOptionActivity : AppCompatActivity() {
             mqttHandler = MqttHandler(this.applicationContext, imageView)
             mqttHandler!!.connectToMqttBroker()
 
-            imageView.setOnClickListener{
+            imageView.setOnClickListener {
                 window.dismiss()
             }
             window.showAsDropDown(mCameraButton)
         }
-        // Cleaning start bagfull progressBar
-        progressBar = findViewById<ProgressBar>(R.id.progressBar) as ProgressBar
-        val btn= findViewById<Button>(R.id.start)
 
-        //on click for button
-        btn.setOnClickListener {
-            var progressStatus=0
-            var handler= Handler()
-            // var run=true
+        handler = Handler(Handler.Callback {
+            var progressBar: ProgressBar = findViewById<ProgressBar>(R.id.progressBar)
+            if (isStarted && progressStatus <100) {
+                progressStatus++
+                if(progressStatus==100){
+                    Toast.makeText(applicationContext, "Waste Bag is full, please empty bag", Toast.LENGTH_LONG).show()
 
-            Thread(Runnable {
-                // val progressStatus = Integer.valueOf(mBagfull.text.toString())
-
-                while (progressStatus < 100) {
-                    // progressBar!!.progress = progressStatus+1
-                    progressStatus+=1
-
-                    Thread.sleep(1000)
-                    handler.post{
-                        progressBar!!.progress=progressStatus
-
-                    }
                 }
-            }).start()
+            }
+            progressBar.progress = progressStatus
+            var progressView=findViewById<TextView>(R.id.textViewProgress)
+            progressView.text = "Bag full ${progressStatus}% "
+            handler?.sendEmptyMessageDelayed(0, 100)
+
+            true
+        })
+
+        handler?.sendEmptyMessage(0)
 
 
+        btn1.setOnClickListener {
+            isStarted = ! isStarted
 
         }
-        //method invoke to drive car via MQTT
-        val btn2=findViewById<Button>(R.id.empty_b)
+        //empty the bag
         btn2.setOnClickListener {
-            progressBar!!.progress=0
-            // toast message ="Empth bag has been pushed"
-            //so babfull progress can start again.
-            // counting keep going( looping )so we pause that until message is arrived,
-
-
+            progressStatus = 0
+            isStarted = false
         }
 
 
@@ -119,18 +131,30 @@ class ManualOptionActivity : AppCompatActivity() {
                 }
             }
         })
+
     }
-    private fun turnForwards ( angle: Int) : Int{
+
+    private fun turnForwards(angle: Int): Int {
         val angle = 90 - angle
         return angle
     }
 
-    private fun turnBackwards ( angle: Int) : Int{
-        val angle =  angle - 270
+    private fun turnBackwards(angle: Int): Int {
+        val angle = angle - 270
         return angle
     }
 
     private fun sendMovement(newSpeed: Int, newAngle: Int) {
-            mqttHandler?.drive(newSpeed,newAngle,"")
-        }
-     }
+        mqttHandler?.drive(newSpeed, newAngle, "")
+    }
+
+}
+
+
+
+
+
+
+
+
+
